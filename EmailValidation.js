d17dc378861f62f3acdee83927ed42e77161bfec
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-17 20:55:11 +0530

    [Akshay] -> Validate Email address using regex with mandatory @,dotand special characters

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,4 +1,4 @@
-const emailRegex = RegExp("^abc.*$");
+const emailRegex = RegExp("^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$");
 
 function emailTest(email){
     if(emailRegex.test(email))console.log("Valid Email");
@@ -6,13 +6,13 @@ function emailTest(email){
 }
 
 try{
-    emailTest("abc@gmail.com");
+    emailTest("arijit@yahoo.co.in");
 }catch(e){
     console.error(e);
 }
 
 try{
-    emailTest("@gmail.com");
+    emailTest("abc@bridgelabz.co.in.in");
 }catch(e){
     console.error(e);
 }
