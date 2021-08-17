3b96aae57975f4d5f7fb53c8303170cdae5a8c10
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-17 21:04:06 +0530

    [Akshay] Added->Regex Pattern For PinCode Which Start, End With Number and Allows Space At The Middle Of Pin

---
+++ PinCodeValidation.js
@@ -0,0 +1,30 @@
+const pinRegex = RegExp("^([1-9])(\\S){2}(\\s)?\\S{2}[0-9]$");
+
+function pinCodeTest(pinCode){
+    if(pinRegex.test(pinCode))console.log("Valid Pin Code");
+    else throw "Pin Code is Invalid";
+}
+
+try{
+    pinCodeTest("400088");
+}catch(e){
+    console.error(e);
+}
+
+try{
+    pinCodeTest("400 088");
+}catch(e){
+    console.error(e);
+}
+
+try{
+    pinCodeTest("a40088");
+}catch(e){
+    console.error(e);
+}
+
+try{
+    pinCodeTest("a4008b");
+}catch(e){
+    console.error(e);
+}
